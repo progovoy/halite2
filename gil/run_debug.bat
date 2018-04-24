92277418fdae468c4a7105870cf6035eaed30a61
GilIzi <gil.izidor@gmail.com>
2018-04-24 11:27:21 +0300

    Fix for my external machine

--- gil/run_debug.bat
+++ gil/run_debug.bat
@@ -1,1 +1,1 @@
-start "" "C:\Users\User\PycharmProjects\halite2\gil\halite.exe" -r -t -d "240 160" "python botproxy.py 1337" "python MyBot.py NODEBUG"
+start "" "C:\Users\gill\Documents\GitHub\halite2\gil\halite.exe" -r -t -d "240 160" "python botproxy.py 1337" "python MyBot.py NODEBUG"
